82952aa45aecb149ec0c4fb62066f63c3b80e6c4
Mariusz Donocik <1987mariuszd1987@wp.pl>
2023-12-21 23:13:36 +0100

    Program sprawdzający czy słowo zawiera literę, cyfrę i znak specjalny

--- Program.cs
+++ Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Wprowadź słowo:");
+            string slowo = Console.ReadLine();
+            // Usunięcie spacji z puli znaków specjalnych
+            slowo = UsunSpacjeZeZnakowSpecjalnych(slowo);
+
+            // Usunięcie wykrzyknika z puli znaków specjalnych
+            slowo = UsunZnakSpecjalny(slowo, '!');
+
+            bool zawieraZnakSpecjalny = CzyZawieraZnakSpecjalny(slowo);
+            bool zawieraDuzaLitere = CzyZawieraDuzaLitere(slowo);
+
+            if (!zawieraZnakSpecjalny)
+            {
+                if (!zawieraDuzaLitere)
+                {
+                    Console.WriteLine("Słowo nie zawiera ani znaku specjalnego ani wielkiej litery");
+                }
+                else
+                {
+                    Console.WriteLine("Słowo nie zawiera znaku specjalnego");
+                }
+            }
+            else
+            {
+                if (!zawieraDuzaLitere)
+                {
+                    Console.WriteLine("Słowo nie zawiera wielkiej litery");
+                }
+                else
+                {
+                    Console.WriteLine("OK");
+                    break;
+                }
+            }
+        }
+    }
+    static bool CzyZawieraZnakSpecjalny(string slowo)
+    {
+        foreach (char znak in slowo)
+        {
+            if (!char.IsLetterOrDigit(znak))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    static bool CzyZawieraDuzaLitere(string slowo)
+    {
+        foreach (char znak in slowo)
+        {
+            if (char.IsUpper(znak))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    static string UsunSpacjeZeZnakowSpecjalnych(string slowo)
+    {
+        // Usunięcie spacji z puli znaków specjalnych
+        return slowo.Replace(" ", "");
+    }
+
+
+    static string UsunZnakSpecjalny(string slowo, char znakSpecjalny)
+    {
+        // Usunięcie określonego znaku specjalnego
+        return slowo.Replace(znakSpecjalny.ToString(), "");
+    }
+}
